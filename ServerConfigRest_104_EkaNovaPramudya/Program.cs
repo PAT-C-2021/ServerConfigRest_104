e0849ea253bd8cb0a51e9d1360abb211dc7b9bfe
Eka Nova Pramudya <78529450+pramek008@users.noreply.github.com>
2021-12-31 22:33:23 +0700

    EkaNovaPramudya-20190140104

---
+++ ServerConfigRest_104_EkaNovaPramudya/Program.cs
@@ -0,0 +1,32 @@
+﻿using ServiceRest_20190140104_EkaNovaPramudya;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConfigRest_104_EkaNovaPramudya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = null;
+
+            try
+            {
+                host = new ServiceHost(typeof(TI_UMY));
+                host.Open();
+                Console.WriteLine("Server Siap....!");
+                Console.ReadLine();
+                host.Close();
+            }catch(Exception e)
+            {
+                host = null;
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
